cf111b99b5bb4d30e8566a5ee9ae07e2d74c49ca
gogoPAN <panpchen@gmail.com>
2021-02-03 07:01:54 +0800

    07:00

--- assets/Script/Game.ts
+++ assets/Script/Game.ts
@@ -12,17 +12,47 @@ cc.macro.ENABLE_TRANSPARENT_CANVAS = true;
 export default class Game extends cc.Component {
   @property(cc.VideoPlayer)
   videoPlayer: cc.VideoPlayer = null;
+  @property([cc.Node])
+  btns: cc.Node[] = [];
+
+  private _allVideos: cc.Asset[] = [];
   onLoad() {
     this.videoPlayer.node.on("completed", this._onCompleted, this);
     this.videoPlayer.node.on("ready-to-play", this._onReadyToPlay, this);
+    // 预加载videos所有视频
+    cc.resources.preloadDir("Videos", cc.Asset);
+    cc.resources.loadDir("Videos", cc.Asset, (err, assets) => {
+      if (err) {
+        cc.error(err);
+      } else {
+        cc.log(assets);
+        this._allVideos = assets;
+      }
+    });
   }
 
   _onCompleted(event) {
-    cc.error("completed: ", event.detail);
+    cc.error("completed: ", event);
   }
 
   _onReadyToPlay(event) {
-    cc.error("ready: ", event.detail);
+    cc.error("ready: ", event);
+    this._playVideo(5);
+  }
+
+  _playVideo(num) {
+    this.videoPlayer.clip = this._allVideos[num - 1];
     this.videoPlayer.play();
+  }
+
+  onClickBtn(evt, parm) {
+    switch (parm) {
+      case "left":
+        this._playVideo(1);
+        break;
+      case "right":
+        this._playVideo(2);
+        break;
+    }
   }
 }
